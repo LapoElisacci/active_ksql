05c80962a82cc2bce5be06e73b2a4e1ca9f7ae8f
Lapo <lapoelisacci@gmail.com>
2021-12-26 11:47:25 +0100

    First commit

---
+++ spec/active_ksql_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ActiveKsql do
+  it "has a version number" do
+    expect(ActiveKsql::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
